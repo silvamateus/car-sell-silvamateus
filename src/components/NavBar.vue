0d136910e8a6687ea79bae457456de820dfc28f8
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-21 15:14:00 -0300

    add style

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -32,4 +32,44 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+button {
+  color: white !important;
+}
+/* Nav Bar*/
+.nav-bar {
+  display: flex;
+  position: fixed;
+  width: 100vw;
+  align-items: center;
+  padding: 1rem;
+}
+/* Menu*/
+.menu {
+  display: flex;
+  justify-content: flex-end;
+  flex-basis: 90%;
+  position: relative;
+}
+.link-list {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: rgb(221, 235, 172);
+  padding: 1rem;
+  position: fixed;
+  right: 4rem;
+  top: 3rem;
+  border-radius: 0.5rem;
+  color: #000;
+}
+.link-list a {
+  padding: 0 1rem;
+  text-decoration: none;
+  color: inherit;
+}
+.logo {
+  width: 8rem;
+}
+
+</style>
