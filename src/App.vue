fc0172a60a9bc838f89877d1fadfc1c8683182c1
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-22 16:11:11 -0300

    add Carousel component to App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app" @click="show = false">
     <nav-bar :show="show" @showMenu="show = $event" />
+    <carousel />
   </div>
 </template>
 
 <script>
+import Carousel from "./components/Carousel.vue";
 import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "App",
-  components: { NavBar },
+  components: { NavBar, Carousel },
   data() {
     return {
       show: false,
