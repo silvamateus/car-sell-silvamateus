ed9125c7aef6d792d5598fa8775534033bf5a434
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-24 18:19:38 -0300

    create api file

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+/*
+ * Api call
+ * Required: uri, method, params
+ */
+const api = async (uri, method, params) =>
+  await fetch(
+    `https://us-central1-spotz-prod.cloudfunctions.net/function-sell-my-car/${uri}`,
+    {
+      method,
+      params,
+    }
+  );
+/*
+ * GET method
+ * Required: none
+ * Optional: key, value
+ */
+export const get = async (key = "", value = "") => {
+  return await api("announcements", "GET", { [key]: value });
+};
