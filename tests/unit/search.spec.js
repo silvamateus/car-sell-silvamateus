8ab0e8afb42503cbd90c8687330bee7fdaab9dc7
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-27 08:40:59 -0300

    add SearchBar render test

--- tests/unit/search.spec.js
+++ tests/unit/search.spec.js
@@ -1,6 +1,7 @@
 import { mount } from "@vue/test-utils";
 import Search from "@/components/Search";
 import Card from "@/components/Search/Card.vue";
+import SearchBar from "@/components/Search/SearchBar.vue";
 
 describe("Search/index.vue", () => {
   let wrapper;
@@ -11,6 +12,10 @@ describe("Search/index.vue", () => {
 
   it("should render", () => {
     expect(wrapper.exists()).toBeTruthy();
+  });
+  it("should render SearchBar", () => {
+    const searchBar = wrapper.findComponent(SearchBar);
+    expect(searchBar.exists()).toBeTruthy();
   });
   it("should render Card", () => {
     const card = wrapper.findComponent(Card);
