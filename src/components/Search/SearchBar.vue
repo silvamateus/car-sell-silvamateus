0e4658f634369ea19b1f51161f5abfa420f722b7
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-28 13:36:42 -0300

    change searchbar inputs position

--- src/components/Search/SearchBar.vue
+++ src/components/Search/SearchBar.vue
@@ -1,22 +1,28 @@
 <template>
   <form class="search-bar" @submit.prevent="toFilter">
     <div>
-      <label for="minPrice">Preço Mín.</label>
-      <input type="number" id="minPrice" v-model="minPrice" />
+      <div>
+        <label for="minPrice">Preço Mín.</label>
+        <input type="number" id="minPrice" v-model="minPrice" />
+      </div>
+      <div>
+        <label for="maxPrice">Preço Máx.</label>
+        <input type="number" id="maxPrice" v-model="maxPrice" />
+      </div>
     </div>
     <div>
-      <label for="maxPrice">Preço Máx.</label>
-      <input type="number" id="maxPrice" v-model="maxPrice" />
+      <div>
+        <label for="minKm">Mín. Kms</label>
+        <input type="number" id="minKm" v-model="minKm" />
+      </div>
+      <div>
+        <label for="maxKm">Máx. Kms</label>
+        <input type="number" id="maxKm" v-model="maxKm" />
+      </div>
     </div>
     <div>
-      <label for="minKm">Mín. Kms</label>
-      <input type="number" id="minKm" v-model="minKm" />
+      <button type="submit">Filtrar</button>
     </div>
-    <div>
-      <label for="maxKm">Máx. Kms</label>
-      <input type="number" id="maxKm" v-model="maxKm" />
-    </div>
-    <button type="submit">Filtrar</button>
   </form>
 </template>
 
@@ -51,36 +57,42 @@ export default {
 </script>
 
 <style scoped>
-/* Search Bar */
-.search-bar {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  flex-direction: column;
-}
-.search-bar > div {
-  display: flex;
-  justify-content: space-between;
-  padding-right: 1rem;
-  padding-bottom: 1rem;
-  width: 12rem;
-}
 button {
-  width: 4rem;
-  background-color: var(--green);
+  width: 5rem;
+  background-color: var(--white);
   padding: 0.5rem;
   font-size: calc(0.8rem + var(--increase-font));
   border-radius: var(--default-radius);
   transition: background-color 500ms ease-in-out;
+  background-color: var(--button-color);
+  color: var(--white);
 }
 button:hover {
-  background-color: var(--dark-green);
+  background-color: var(--button-hover-color);
 }
+/* Search Bar */
+.search-bar {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #a5abbd;
+  width: 100%;
+}
+.search-bar > div {
+  display: flex;
+  justify-content: space-between;
+  width: 15rem;
+  margin: auto;
+  padding-bottom: 2rem;
+}
+
 .search-bar label {
   font-size: calc(0.8rem + var(--increase-font));
 }
 .search-bar input {
-  border: 2px solid var(--green);
+  border: 2px solid var(--white);
   border-radius: 0.5rem;
   width: 5rem;
 }
