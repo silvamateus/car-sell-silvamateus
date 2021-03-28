93ccd79236249b7c1520fdd5c279d4b6aefd4b65
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-28 19:44:59 -0300

    Remove Card from Search

--- tests/unit/search.spec.js
+++ tests/unit/search.spec.js
@@ -1,6 +1,5 @@
 import { mount } from "@vue/test-utils";
 import Search from "@/components/Search";
-import Card from "@/components/Search/Card.vue";
 import SearchBar from "@/components/Search/SearchBar.vue";
 
 global.fetch = jest.fn(() =>
@@ -52,14 +51,5 @@ describe("Search/index.vue", () => {
       minKm: 0,
     });
     expect(searchBar.emitted().toSubmit).toBeTruthy;
-  });
-
-  it("should render Card", () => {
-    const card = mount(Card, {
-      propsData: {
-        car: { name: "", brand: "", image: "", id: "", price: 0 },
-      },
-    });
-    expect(card.exists()).toBeTruthy();
   });
 });
